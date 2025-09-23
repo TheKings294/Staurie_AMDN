247318faa2ec56f9f0a99b81e258d79d95622dd5
TheKings294 <98644094+TheKings294@users.noreply.github.com>
2025-09-23 14:16:42 +0200

    base main php

--- main.php
+++ main.php
@@ -1,4 +1,7 @@
 <?php
 
-echo("Hello World")
+use Jugid\Staurie\Component\Console\Console;
+use Jugid\Staurie\Component\Menu\Menu;
+use Jugid\Staurie\Component\PrettyPrinter\PrettyPrinter;
 
+require_once __DIR__ . '/vendor/autoload.php';
