d43f03be9839786acca3e70ae1228de97cc03ca5
TheKings294 <98644094+TheKings294@users.noreply.github.com>
2025-09-24 11:36:37 +0200

    edit main

--- main.php
+++ main.php
@@ -5,6 +5,7 @@ use Jugid\Staurie\Component\Console\Console;
 use Jugid\Staurie\Component\Menu\Menu;
 use Jugid\Staurie\Component\PrettyPrinter\PrettyPrinter;
 use Jugid\Staurie\Staurie;
+use Jugid\Staurie\Component\Map\Map;
 
 require_once __DIR__ . '/vendor/autoload.php';
 define("SRC_DIR", __DIR__);
@@ -24,8 +25,7 @@ $dialogue->configuration([
 ]);
 
 $dispatcher = $container->dispatcher();
+/*$result = $dispatcher->dispatch('scenario.start', ['scenario' => 'scenario1']);
+$result = $dispatcher->dispatch('npc.talk', ['npc' => 'Professor Plum', 'dialogue' => 'dialogue1']);*/
 
 $staurie->run();
-
-$result = $dispatcher->dispatch('scenario.start', ['scenario' => 'scenario1']);
-$result = $dispatcher->dispatch('npc.talk', ['npc' => 'Professor Plum', 'dialogue' => 'dialogue1']);
